382a1a9795e5aba0626a278988bf1a6898c52df3
Emerson Alves <emersondasilva.alves99@gmail.com>
2022-03-10 16:42:48 -0300

    [Requisito 2] - Cria estrutura de pastas, rotas e página home

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+export default class index extends Component {
+  render() {
+    return (
+      <div>
+        <input />
+        <p data-testid="home-initial-message">
+          Digite algum termo de pesquisa ou escolha uma categoria.
+        </p>
+      </div>
+    );
+  }
+}
