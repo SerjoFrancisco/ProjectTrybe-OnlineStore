6958d08604681683ada5f9af31d083f8f308d1b4
Talita Afonso <talita.afonso24@gmail.com>
2022-03-11 19:07:21 +0000

    Requisito 4 - Lista as categorias de produtos disponíveis via API

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,8 +1,22 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { getCategories } from '../../services/api';
 
 export default class Home extends Component {
+  constructor() {
+    super();
+    this.state = {
+      categorias: [],
+    };
+  }
+
+  async componentDidMount() {
+    const categorias = await getCategories();
+    this.setState({ categorias });
+  }
+
   render() {
+    const { categorias } = this.state;
     return (
       <div>
         <input />
@@ -10,6 +24,19 @@ export default class Home extends Component {
           Digite algum termo de pesquisa ou escolha uma categoria.
         </p>
         <Link to="/cart" data-testid="shopping-cart-button">Cart</Link>
+        <section>
+          {
+            categorias.map(({ name, id }) => (
+              <button
+                key={ id }
+                type="button"
+                data-testid="category"
+              >
+                { name }
+              </button>))
+          }
+        </section>
+
       </div>
     );
   }
