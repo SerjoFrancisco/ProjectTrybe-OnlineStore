3270d920fc5ff0e9ea682179730fec8e9eaefdeb
Caique Ferian <caiquegsf@gmail.com>
2022-03-15 17:40:58 -0300

    Requisito 11- adiciona formulario para avaliacao do produto pelo usuario

--- src/pages/ProductDetails/index.jsx
+++ src/pages/ProductDetails/index.jsx
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import { getProductFromId } from '../../services/api';
 import addProductToCart from '../../helpers/addProductToCart';
+import Forms from '../../components/Forms/Forms';
 
 export default class ProductDetails extends Component {
   constructor() {
@@ -10,19 +11,47 @@ export default class ProductDetails extends Component {
     this.state = {
       productId: '',
       product: {},
+      email: '',
+      messageDescription: '',
+      starRating: '',
+      savedRates: [],
     };
   }
 
   componentDidMount() {
     const { match: { params } } = this.props;
     const { productId } = params;
-    this.setState({ productId }, () => this.productOnState());
+    const savedRates = JSON.parse(localStorage.getItem('rate'));
+    this.setState({ productId, savedRates }, () => this.productOnState());
   }
 
   handleClick = () => {
     const { product: { title, thumbnail, price, id } } = this.state;
     const infos = { title, thumbnail, price, id, amount: 0 };
     addProductToCart(infos);
+  }
+
+  saveRate = () => {
+    const { email, messageDescription, starRating } = this.state;
+    const savedRates = JSON.parse(localStorage.getItem('rate'));
+    const toSave = savedRates
+      ? [...savedRates, { email, messageDescription, starRating }]
+      : [{ email, messageDescription, starRating }];
+    localStorage.setItem('rate',
+      JSON.stringify(toSave));
+    this.setState(
+      { savedRates: toSave,
+        email: '',
+        messageDescription: '',
+        starRating: '' },
+    );
+  }
+
+  handleChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    });
   }
 
   async productOnState() {
@@ -32,7 +61,7 @@ export default class ProductDetails extends Component {
   }
 
   render() {
-    const { product } = this.state;
+    const { product, email, messageDescription, savedRates } = this.state;
     return (
       <div>
         <Link to="/cart" data-testid="shopping-cart-button">Cart</Link>
@@ -52,6 +81,22 @@ export default class ProductDetails extends Component {
         >
           Add to Cart
         </button>
+        <Forms
+          messageDescription={ messageDescription }
+          onChange={ this.handleChange }
+          email={ email }
+          onClick={ this.saveRate }
+        />
+
+        {
+          savedRates?.map((item, i) => (
+            <div key={ i }>
+              <p>{item.email}</p>
+              <p>{item.messageDescription}</p>
+              <p>{item.starRating}</p>
+            </div>
+          ))
+        }
       </div>
     );
   }
